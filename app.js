041681b6a8a432a62358a4291760880170920556
Saket Gulhane <saket.gulhane291@gmail.com>
2023-08-23 21:36:23 +0530

    natours-added routes and controllers

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!', app: 'natours' });
+// 1. Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware👋');
+  next();
 });
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2. Route handlers
+
+// 3. Routes
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4. Start Server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.....`);
