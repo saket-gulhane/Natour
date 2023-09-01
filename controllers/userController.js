2d94ebb3dc50ee0118f73b60fa57db67e1102975
Saket Gulhane <saket.gulhane291@gmail.com>
2023-09-01 19:28:42 +0530

    Natours-auth functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedField) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedField.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -9,6 +19,41 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     status: 'success',
     results: users.length,
     data: { users },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create error if user POSts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+
+  // 2. Filtered out unwanted field name that are not allowed (eg. password)
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3. Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: { user: updatedUser },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+// const { resetPassword } = require('../controllers/authController');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +23,11 @@ const userSchema = new mongoose.Schema({
   photo: {
     type: String,
   },
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
@@ -39,6 +46,13 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -48,8 +62,21 @@ userSchema.pre('save', async function (next) {
   // hash the password with cost 12
   this.password = await bcrypt.hash(this.password, 12);
 
-  // delete the passwordCOnfirm field
+  // delete the passwordConfirm field
   this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
@@ -71,6 +98,21 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
 
   // False means NOT change
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
